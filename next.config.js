6d28ba111a075b787da7a3de704f7c046603db5b
Amrit Sharma <amrit.sharma@bik.ai>
2026-01-04 13:14:45 +0530

    Remove basePath for root deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
   images: {
     unoptimized: true,
   },
-  // Update this with your GitHub repo name
-  basePath: process.env.NODE_ENV === 'production' ? '/portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio/' : '',
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
