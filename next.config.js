cafda2b1a266299320c4933cfe434fd076de2a75
Amrit Sharma <amrit.sharma@bik.ai>
2026-01-04 13:16:45 +0530

    minor

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  // Update this with your GitHub repo name
+  basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/portfolio/" : "",
 };
 
 module.exports = nextConfig;
